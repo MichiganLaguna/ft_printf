f00f53e4d6c5357ca0ee1d8a6428aca6681f13b6
nriviere <nriviere@student.42.fr>
2022-12-15 06:55:07 +0100

    oof

--- capture_stdout.c
+++ capture_stdout.c
@@ -1,18 +1,28 @@
 #include "test.h"
 
-r_value *single_capture_int(int (*f)(const char *str, ...), char *str, int arg)
+r_value *_capture(int (*f)(const char *str, ...), char *c, ...)
 {
     int pipes[2];
     int bread;
     int tmp_stdout;
-    r_value    *out;
+    r_value *out;
+    va_list arg;
 
     fflush(stdout);
     tmp_stdout = dup(fileno(stdout)); // make a copy of stdout so we don't lose it
     pipe(pipes); // create a pipe
     dup2(pipes[1], fileno(stdout)); // copy stdout to pipe and close stdout
 
-    bread = f(str, arg);  // output to stdout is instead captured to pipe
+    va_start(arg, c);
+
+    if (*c == 's')
+        bread = f(c, va_arg(arg, char *));  // output to stdout is instead captured to pipe
+    else if (*c == 'p' || *c == 'u' || *c == 'x' || *c == 'X' || *c == '%')
+        bread = f(c, va_arg(arg, unsigned long));
+    else if (*c == 'd' || *c == 'i')
+        bread = f(c, va_arg(arg, int));
+
+    va_end(arg);
 
     fflush(stdout); // clean 
     dup2(tmp_stdout, fileno(stdout)); // restore stdout
@@ -23,50 +33,47 @@ r_value *single_capture_int(int (*f)(const char *str, ...), char *str, int arg)
     return (out);
 }
 
-r_value *single_capture_uint(int (*f)(const char *str, ...), char *str, size_t arg)
+r_value *single_capture(int (*f)(const char *str, ...), char *str, ...)
 {
-    int pipes[2];
-    int bread;
-    int tmp_stdout;
     r_value    *out;
+    int     i;
+    va_list args;
 
-    fflush(stdout);
-    tmp_stdout = dup(fileno(stdout)); // make a copy of stdout so we don't lose it
-    pipe(pipes); // create a pipe
-    dup2(pipes[1], fileno(stdout)); // copy stdout to pipe and close stdout
-
-    bread = f(str, arg);  // output to stdout is instead captured to pipe
-
-    fflush(stdout); // clean 
-    dup2(tmp_stdout, fileno(stdout)); // restore stdout
-    out = malloc(sizeof(r_value));
-    out->str = calloc(sizeof(char), bread + 1);
-    read(pipes[0], out->str, bread); // read the exact string length
-    out->bread = bread;
-    return (out);
+    i = 0;
+    va_start(args, str);
+    while (str[i])
+    {
+        if (str[i] == 's')
+            out = _capture(f, "s", va_arg(args, char *));
+        else if (str[i] == 'p')
+            out = _capture(f, "p", va_arg(args, void *));
+        else if (str[i] == 'u')
+            out = _capture(f, "u", va_arg(args, unsigned int));
+        else if (str[i] == 'x')
+            out = _capture(f, "x", va_arg(args, unsigned long));
+        else if (str[i] == 'X')
+            out = _capture(f, "X", va_arg(args, unsigned long));
+        else if (str[i] == 'd')
+            out = _capture(f, "d", va_arg(args, int));
+        else if (str[i] == 'i')
+            out = _capture(f, "i", va_arg(args, int));
+        i++;
+    }
+    va_end(args);
+    return (out);    
 }
 
-r_value *single_capture_str(int (*f)(const char *str, ...), char *str, char *arg)
+void    free_r_value(int nbr, ...)
 {
-    int pipes[2];
-    int bread;
-    int tmp_stdout;
-    r_value    *out;
+    va_list args;
+    r_value *tmp;
 
-    fflush(stdout);
-    tmp_stdout = dup(fileno(stdout)); // make a copy of stdout so we don't lose it
-    pipe(pipes); // create a pipe
-    dup2(pipes[1], fileno(stdout)); // copy stdout to pipe and close stdout
-
-    bread = f(str, arg);  // output to stdout is instead captured to pipe
-
-    fflush(stdout); // clean 
-    dup2(tmp_stdout, fileno(stdout)); // restore stdout
-    out = malloc(sizeof(r_value));
-    out->str = calloc(sizeof(char), bread + 1);
-    read(pipes[0], out->str, bread); // read the exact string length
-    out->bread = bread;
-    return (out);
+    va_start(args, nbr);
+    while (nbr--)
+    {
+        tmp = va_arg(args, r_value*);
+        free(tmp->str);
+        free(tmp);
+    }
+    va_end(args);
 }
-
-void    free_r_value()
--- test.h
+++ test.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdarg.h>
 
 
 typedef struct
